ea0f4a1c8f454366e2fb83770ad72bef30cebaaf
ShubhamPal19 <shubhampal2309@gmail.com>
2021-08-26 23:36:04 +0530

    Create welcome.java

---
+++ CUSTOM_PACKAGE_IN_JAVA/welcome.java
@@ -0,0 +1,14 @@
+package introduction.welcome;
+
+public class welcome {
+    
+    public static void main(String[] args) {
+        
+
+        System.out.println("PRint this as welcome");
+    }
+}
+
+// use this command in terminal to complile all the files and put them in a custom package
+
+// -------->     javac -d . *.java
