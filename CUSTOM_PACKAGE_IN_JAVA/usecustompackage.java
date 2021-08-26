f1fd3584b85ce485acea858dc5e6eb67042d76ee
ShubhamPal19 <shubhampal2309@gmail.com>
2021-08-26 23:35:53 +0530

    Create usecustompackage.java

---
+++ CUSTOM_PACKAGE_IN_JAVA/usecustompackage.java
@@ -0,0 +1,15 @@
+
+package introduction.welcome;
+
+
+import introduction.welcome.source;
+
+
+public class usecustompackage {
+
+
+    public static void main(String[] args) {
+        System.out.println("source class file imported");
+    }
+    
+}
