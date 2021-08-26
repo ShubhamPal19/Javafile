607850668e23e2a98e546b77a67c97976e199c4f
ShubhamPal19 <shubhampal2309@gmail.com>
2021-08-26 23:35:59 +0530

    Create source.java

---
+++ CUSTOM_PACKAGE_IN_JAVA/source.java
@@ -0,0 +1,8 @@
+package introduction.welcome;
+public class source {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("THis is the source class to be used in different java files");
+    }
+}
